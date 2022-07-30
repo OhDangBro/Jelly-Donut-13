509e8bb88b448970740cd41b3439b36102bc7001
Will <103394466+OhDangBro@users.noreply.github.com>
2022-07-30 17:34:44 -0400

    databases created

--- models/index.js
+++ models/index.js
@@ -17,4 +17,5 @@ module.exports = {
   Category,
   Tag,
   ProductTag,
+  
 };
--- server.js
+++ server.js
@@ -14,3 +14,8 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+
+
+// export PATH=${PATH}:/usr/local/mysql/bin
+/// mysql -u root -p
