5add41765d27b307a7fc5f2b26105ef2b30ad650
Will <103394466+OhDangBro@users.noreply.github.com>
2022-08-03 19:30:43 -0400

    project 13 completedgit add . All insomnia runs, schema and seed also run

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,12 @@ DROP DATABASE IF EXISTS ecommerce_db;
 CREATE DATABASE ecommerce_db;
 USE ecommerce_db;
 
+/* 
+Step 1: export PATH=${PATH}:/usr/local/mysql/bin
+Step 2: myswl -u root -p
+Step 3: source db/schema.sql
+Step 4: SHOW DATABASES;
+Step 5: USE ecommerce_db;
+Step 6: exit;
+Step 7: npm run seed
+Step 8: npm run start
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -27,6 +27,20 @@ router.get('/:id', (req, res) => {
 });
 ///////////////////////FIND TAG BY ID END//////////////
 
+////////////////CREATE TAG START //////////////////////
+router.post('/', (req, res) => {
+  // create a new tag
+  Tag.create({
+      tag_name: req.body.tag_name
+  })
+      .then(dbTagData => res.json(dbTagData))
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+/////////////////////CREATE TAG END//////////////////////
+
 
 
 /////////////////////// PUT TAG START //////////////////////////
